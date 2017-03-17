f22692922a9b06af2096f8031fc5ac8e819a695b
OgliariNatan <natanogliari@gmail.com>
2017-03-17 15:03:02 -0300

    LDR&LM35

--- Software/Arduino/LDR_TEEMP/LDR_TEEMP.ino
+++ Software/Arduino/LDR_TEEMP/LDR_TEEMP.ino
@@ -1,6 +1,6 @@
 const int ledPin = 13; //Led interno da placa
-const int ldrPin = A6; //LDR no pino analígico 8 
-const int LM35 = A1;
+const int LDR = A3; //LDR no pino analígico 8 
+const int LM35 = A6;
 
 void setup() {
  pinMode(ledPin,OUTPUT); //define a porta 7 como saída
@@ -10,14 +10,16 @@ void setup() {
 void loop() {
  ///ler o valor do LDR
  int ldrValor = 0;
- ldrValor = analogRead(ldrPin); //O valor lido será entre 0 e 1023
- 
+ int LM35Valor = 0;
+ ldrValor = analogRead(LDR); //O valor lido será entre 0 e 1023
+ LM35Valor = analogRead(LM35); 
  //se o valor lido for maior que 500, liga o led
  if (ldrValor>= 800) digitalWrite(ledPin,HIGH);
  // senão, apaga o led
  else digitalWrite(ledPin,LOW);
  
  //imprime o valor lido do LDR no monitor serial
+ Serial.print ("LDR: ");
  Serial.println(ldrValor);
  delay(100);
 }

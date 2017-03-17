2c2caf3b5822aaf1c8d62466f896509e43a3d87e
OgliariNatan <natanogliari@gmail.com>
2017-03-17 15:54:51 -0300

    LDR&LM35 com bluetooth

--- Software/Arduino/LDR_TEEMP/LDR_TEEMP.ino
+++ Software/Arduino/LDR_TEEMP/LDR_TEEMP.ino
@@ -1,4 +1,4 @@
-#include <SoftwareSerial.h>
+#include <SoftwareSerial.h>//bluetooth
 const int ledPin = 13; //Led interno da placa
 const int LDR = A3; //LDR no pino analígico 8 
 const int LM35 = A6;
@@ -13,33 +13,29 @@ void setup() {
 }
  
 void loop() {
- ///ler o valor do LDR
  int ldrValor = 0;
  float LM35Valor = 0;
- ldrValor = analogRead(LDR); //O valor lido será entre 0 e 1023
+ 
+ ldrValor = analogRead(LDR); //ler o valor do LDR, O valor lido será entre 0 e 1023
  LM35Valor = (float(analogRead(LM35))*5/(1023))/0.01;// Valor dado em °C
- //se o valor lido for maior que 500, liga o led
- if (ldrValor>= 800) digitalWrite(ledPin,HIGH);
- // senão, apaga o led
- else digitalWrite(ledPin,LOW);
  
  //imprime o valor lido do LDR no monitor serial
- Serial.print ("LDR: ");
- Serial.println(ldrValor);
-// imprimi o valor lido do LM35 no monitor serial
- Serial.print ("LM35: ");
- Serial.println(LM35);
+ //Serial.print ("LDR: ");
+ //Serial.println(ldrValor);
+ // imprimi o valor lido do LM35 no monitor serial
+ //Serial.print ("LM35: ");
+ //Serial.println(LM35);
  
-BTserial.print("Temparatura");//Sensor 1
-BTserial.print(",");
-BTserial.print(LM35Valor);//Sensor 2
-BTserial.print(",");
-BTserial.print("°C & LUX");//Sensor 3
-BTserial.print(",");
-BTserial.print("LUX");//Sensor 4
-BTserial.print(",");
-BTserial.print(ldrValor);//Sensor 5
-BTserial.print(";");
+  BTserial.print("Temparatura");//Sensor 1
+  BTserial.print(",");
+  BTserial.print(LM35Valor);//Sensor 2
+  BTserial.print(",");
+  BTserial.print("°C & LUX");//Sensor 3
+  BTserial.print(",");
+  BTserial.print("LUX");//Sensor 4
+  BTserial.print(",");
+  BTserial.print(ldrValor);//Sensor 5
+  BTserial.print(";");
  
- delay(500);
+  delay(500);
 }

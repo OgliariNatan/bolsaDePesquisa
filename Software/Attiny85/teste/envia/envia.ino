b2f53851f00e61a7394f4c12969a47b0e577c751
OgliariNatan <natanogliari@gmail.com>
2017-05-22 20:33:33 -0300

    PCB

---
+++ Software/Attiny85/teste/envia/envia.ino
@@ -0,0 +1,18 @@
+#include <RF24Network.h>
+#include <RF24Network_config.h>
+#include <Sync.h>
+#include <RF24.h>
+#include "tinySPI.h"
+
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}

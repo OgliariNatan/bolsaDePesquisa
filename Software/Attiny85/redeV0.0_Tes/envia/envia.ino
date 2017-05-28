4f026a52c8fb8cfe476cc7b700e7e2e4b8245e23
OgliariNatan <natanogliari@gmail.com>
2017-05-28 17:50:38 -0300

    Codigo do attiny85 com o modulo nrf24l01 em rede, falta o teste na pratica para ver o seu funcionamento e falta a organização das mensagens

---
+++ Software/Attiny85/redeV0.0_Tes/envia/envia.ino
@@ -0,0 +1,43 @@
+#include <RF24Network.h>
+#include <RF24.h>
+
+RF24 radio(5,5);                    // COLOCAR OS VALORES IGUAIS QUANDO UTILIZAR O ATTINY E A BIBLIOTECA JA IRA RECONHECER COMO MODO DE 3 PINOS
+                                    // O PINO 5 CORRESPONDE AO PINO DE RESET NO ATTINY, COLOQUEI DESSA FORMA PARA DEIXAR BEM CLARO QUE ELE NAO VAI
+                                    // UTILIZAR NENHUM OUTRO PINO QUE NÃO SEJA OS 3 CORRESPONDENTES A COMUNICAÇÃO ICSP :)
+   
+RF24Network network(radio);          // Network uses that radio
+
+const uint16_t this_node = 00;        // Address of our node in Octal format
+const uint16_t other_node = 00;       // Address of the other node in Octal format
+
+const unsigned long interval = 2000; //ms  // How often to send 'hello world to the other unit
+
+unsigned long last_sent;             // When did we last send?
+unsigned long packets_sent;          // How many have we sent already
+
+
+struct  __attribute__ ((__packed__)) message_t {                  // Structure of our payload
+  int id;
+  float temperature;
+  float luminosidade;
+};
+
+void setup(void)
+{
+  radio.begin();
+  network.begin(/*channel*/ 90, /*node address*/ this_node);
+}
+
+void loop() {
+  
+  network.update();                          // Check the network regularly
+  unsigned long now = millis();              // If it's time to send a message, send it!
+  if ( now - last_sent >= interval  )
+  {
+    last_sent = now;
+    message_t message = { millis(), packets_sent++ };
+    RF24NetworkHeader header(/*to node*/ other_node);
+    bool ok = network.write(header,&message,sizeof(message));
+  }
+}
+

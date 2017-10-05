a0beee89c3e1c7066438178cd021e030cebf89b1
Natan Ogliari <natanogliari@gmail.com>
2017-10-04 22:30:36 -0300

    iniciado projeto de transmissor, incompleto

---
+++ Software/C/why_not_TX/why_not_TX/main.c
@@ -0,0 +1,18 @@
+/*
+ * why_not_TX.c
+ *
+ * Created: 04/10/2017 22:27:57
+ * Author : Cliente
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+

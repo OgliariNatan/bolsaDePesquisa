fadb9dd766ebccf5b4d76c976b8bc9925318d076
Natan Ogliari <natanogliari@gmail.com>
2017-10-04 22:23:15 -0300

    recolocado a USART atraves da bibliteca (globalDefine && atmega328), configuração do recptor

--- Software/C/why_not/why_not/main.c
+++ Software/C/why_not/why_not/main.c
@@ -15,6 +15,8 @@
 
 nRF24L01 *setup_rf(void);
 void process_message(char *message);
+void usartSetup(void);
+
 inline void prepare_led_pin(void);
 inline void  set_led_high(void);
 inline void  set_led_low(void);
@@ -24,6 +26,10 @@ volatile bool rf_interrupt = false;
 int main(void) {
 	uint8_t address[5] = { 0x01, 0x01, 0x01, 0x01, 0x01 };
 	prepare_led_pin();
+	usartSetup(); //Configura USART
+	
+	
+	
 	sei();
 	nRF24L01 *rf = setup_rf();
 	nRF24L01_listen(rf, 0, address);
@@ -75,7 +81,7 @@ void process_message(char *message) {
 
 inline void prepare_led_pin(void) {
 	setBit(DDRD,PB0); //configura como saida 
-	
+	setBit(PORTB, PB0);
 	//DDRB |= _BV(PB0);
 	//PORTB &= ~_BV(PB0);
 }
@@ -95,105 +101,12 @@ ISR(INT0_vect) {
 	rf_interrupt = true;
 }
 
-
-
-
-
-/*
-// Configuração do nrf24l01
-nRF24L01 *setup_rf(void);
-void process_message(char *message);
-//FIM configuração nrf24
-//inline void prepare_led_pin(void);
-//inline void  set_led_high(void);
-//inline void  set_led_low(void);
-void initIO(void);
-void initADC(void);
-int readADC(uint8_t val);
-volatile bool rf_interrupt = false;
-volatile bool send_message = false;
-
-
-int main(void)
-{
-	uint8_t address[5] = { 0x01, 0x01, 0x01, 0x01, 0x01 };
-	uint8_t val = 0;
-	initIO();
-    sei();
-    
-	nRF24L01 *rf = setup_rf(); //configura os pinos do modulo nrf24l01
-    nRF24L01_listen(rf, 0, address); //configura o modulo para leitura com endereços pré estabelecidos
-    uint8_t addr[5];
-	
-	
-	uint8_t CONFIG = 0; //VERIFICAR APAGAR
-    nRF24L01_read_register(rf, CONFIG, addr, 1);//configura a verificação das mensagem
-	
-		
-    while (1) {
-		val = readADC(0); //pino 0 das ADC
-		
-		
-    }
+//Configuração da USART
+void usartSetup(void){
+	usartConfig(USART_MODE_ASYNCHRONOUS, USART_BAUD_115200, USART_DATA_BITS_8, USART_PARITY_NONE, USART_STOP_BIT_SINGLE);
+	usartEnableTransmitter();
+	usartEnableReceiver();
+	usartClearReceptionBuffer();
+	usartActivateReceptionCompleteInterrupt();
+	usartStdio();
 }
-
-nRF24L01 *setup_rf(void) {
-	nRF24L01 *rf = nRF24L01_init();
-	rf->ss.port = &PORTB;
-	rf->ss.pin = PB2;
-	rf->ce.port = &PORTB;
-	rf->ce.pin = PB1;
-	rf->sck.port = &PORTB;
-	rf->sck.pin = PB5;
-	rf->mosi.port = &PORTB;
-	rf->mosi.pin = PB3;
-	rf->miso.port = &PORTB;
-	rf->miso.pin = PB4;
-	// interrupt on falling edge of INT0 (PD2)
-	EICRA |= _BV(ISC01);
-	EIMSK |= _BV(INT0);
-	nRF24L01_begin(rf);
-	return rf;
-}
-
-// setup timer to trigger interrupt every second when at 1MHz
-void setup_timer(void) {
-	TCCR1B |= _BV(WGM12);
-	TIMSK1 |= _BV(OCIE1A);
-	OCR1A = 15624;
-	TCCR1B |= _BV(CS10) | _BV(CS11);
-}
-
-// each one second interrupt
-ISR(TIMER1_COMPA_vect) {
-	send_message = true;
-}
-
-// nRF24L01 interrupt
-ISR(INT0_vect) {
-	rf_interrupt = true;
-}
-
-void initIO(){//Configuração de I/O
-	DDRC = 0x00;
-	PORTC = 0xFF;
-}
-
-void initADC() {//Conf ADC
-	ADMUX = (1 << REFS0); // Aref = AVcc
-	ADCSRA = (1 << ADEN) | (1 << ADPS2) | (1 << ADPS0); //prescaler 128
-	
-	
-	DDRC &= ~_BV(2); //INPUT
-	PORTC |= _BV(2); //pull high
-	//...
-	};
-	
-int readADC(uint8_t val){
-	ADMUX = (1 << REFS0) | (val & 0x0f);
-	ADCSRA |= (1<<ADSC);
-	while (ADCSRA & (1<<ADSC));
-	return ADCW;
-}
-
-*/
